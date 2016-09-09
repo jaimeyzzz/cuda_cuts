3a13b5a1511e090d5522230882ec8a0e48ee0c0a
lujm <loyaveforever@gmail.com>
2016-09-09 14:16:42 +0800

    split .cu .h files

--- CudaCuts/CudaCuts.h
+++ CudaCuts/CudaCuts.h
@@ -20,12 +20,10 @@
 #define _CUDACUTS_H_
 
 /*Header files included*/
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 
-//#include <cutil.h>
 #include "cuda_runtime.h"
 #include "cuda.h"
 
@@ -38,6 +36,7 @@ inline void gpuAssert(cudaError_t code, const char *file, int line, bool abort =
 		if (abort) exit(code);
 	}
 }
+
 
 /* ***************************************************
  * Kernels which perform the push, pull and         **
@@ -230,4 +229,143 @@ public:
 	int counter;
 
 };
+
+/* **********************************************************
+* CUDA Cuts kernel functions                              **
+* *********************************************************/
+
+__global__ void
+kernel_push1_atomic(int *g_left_weight, int *g_right_weight, int *g_down_weight, int *g_up_weight,
+int *g_sink_weight, int *g_push_reser, int *g_pull_left, int *g_pull_right, int *g_pull_down,
+int *g_pull_up, int *g_relabel_mask, int *g_graph_height, int *g_height_write,
+int graph_size, int width, int rows, int graph_size1, int width1, int rows1);
+
+__global__ void
+kernel_relabel_atomic(int *g_left_weight, int *g_right_weight, int *g_down_weight, int *g_up_weight,
+int *g_sink_weight, int *g_push_reser, int *g_pull_left, int *g_pull_right, int *g_pull_down,
+int *g_pull_up, int *g_relabel_mask, int *g_graph_height, int *g_height_write,
+int graph_size, int width, int rows, int graph_size1, int width1, int rows1);
+
+__global__ void
+kernel_relabel_stochastic(int *g_left_weight, int *g_right_weight, int *g_down_weight, int *g_up_weight,
+int *g_sink_weight, int *g_push_reser, /*int *g_pull_left, int *g_pull_right, int *g_pull_down, int *g_pull_up, */
+int *g_relabel_mask, int *g_graph_height, int *g_height_write,
+int graph_size, int width, int rows, int graph_size1, int width1, int rows1, int *d_stochastic, int *g_block_num);
+
+__global__ void
+kernel_push2_atomic(int *g_left_weight, int *g_right_weight, int *g_down_weight, int *g_up_weight,
+int *g_sink_weight, int *g_push_reser, int *g_pull_left, int *g_pull_right, int *g_pull_down, int *g_pull_up,
+int *g_relabel_mask, int *g_graph_height, int *g_height_write,
+int graph_size, int width, int rows, int graph_size1, int width1, int rows1);
+
+__global__ void
+kernel_End(int *g_stochastic, int *g_count_blocks, int *g_counter);
+
+
+__global__ void
+kernel_push1_start_atomic(int *g_left_weight, int *g_right_weight, int *g_down_weight, int *g_up_weight,
+int *g_sink_weight, int *g_push_reser,
+int *g_relabel_mask, int *g_graph_height, int *g_height_write,
+int graph_size, int width, int rows, int graph_size1, int width1, int rows1, int *d_relabel, int *d_stochastic, int *d_counter, bool *d_finish);
+
+__global__ void
+kernel_push1_stochastic(int *g_left_weight, int *g_right_weight, int *g_down_weight, int *g_up_weight,
+int *g_sink_weight, int *g_push_reser, /*int *g_pull_left, int *g_pull_right, int *g_pull_down, int *g_pull_up,*/
+int *g_relabel_mask, int *g_graph_height, int *g_height_write,
+int graph_size, int width, int rows, int graph_size1, int width1, int rows1, int *d_stochastic, int *g_block_num);
+
+__global__ void
+kernel_push2_stochastic(int *g_left_weight, int *g_right_weight, int *g_down_weight, int *g_up_weight,
+int *g_sink_weight, int *g_push_reser, int *g_pull_left, int *g_pull_right, int *g_pull_down, int *g_pull_up,
+int *g_relabel_mask, int *g_graph_height, int *g_height_write,
+int graph_size, int width, int rows, int graph_size1, int width1, int rows1, int *d_relabel, int *d_stochastic, int *d_counter, bool *d_finish, int *g_block_num);
+
+__global__ void
+kernel_bfs_t(int *g_push_reser, int  *g_sink_weight, int *g_graph_height, bool *g_pixel_mask,
+int vertex_num, int width, int height, int vertex_num1, int width1, int height1);
+
+__global__ void
+kernel_push_stochastic1(int *g_push_reser, int *s_push_reser, int *g_count_blocks, bool *g_finish, int *g_block_num, int width1);
+
+__global__ void
+kernel_push_atomic2(int *g_terminate, int *g_push_reser, int *s_push_reser, int *g_block_num, int width1);
+
+
+__global__ void
+kernel_push_stochastic2(int *g_terminate, int *g_relabel_mask, int *g_push_reser, int *s_push_reser, int *d_stochastic, int *g_block_num, int width1);
+
+__global__ void
+kernel_push1_start_stochastic(int *g_left_weight, int *g_right_weight, int *g_down_weight, int *g_up_weight,
+int *g_sink_weight, int *g_push_reser,
+int *g_relabel_mask, int *g_graph_height, int *g_height_write,
+int graph_size, int width, int rows, int graph_size1, int width1, int rows1, int *d_relabel, int *d_stochastic, int *d_counter, bool *d_finish);
+
+
+__global__ void
+kernel_bfs(int *g_left_weight, int *g_right_weight, int *g_down_weight, int *g_up_weight,
+int *g_graph_height, bool *g_pixel_mask, int vertex_num, int width, int height,
+int vertex_num1, int width1, int height1, bool *g_over, int *g_counter);
+
+__device__
+void add_edge(int from, int to, int cap, int rev_cap, int type, int *d_left_weight,
+int *d_right_weight, int *d_down_weight, int *d_up_weight);
+
+__device__
+void add_tweights(int i, int cap_source, int  cap_sink, int *d_push_reser, int *d_sink_weight);
+
+__device__
+void add_term1(int i, int A, int B, int *d_push_reser, int *d_sink_weight);
+
+__device__
+void add_t_links_Cue(int alpha_label, int thid, int *d_left_weight, int *d_right_weight,
+int *d_down_weight, int *d_up_weight, int *d_push_reser, int *d_sink_weight,
+int *dPixelLabel, int *dDataTerm, int width, int height, int num_labels);
+
+__device__
+void add_t_links(int alpha_label, int thid, int *d_left_weight, int *d_right_weight,
+int *d_down_weight, int *d_up_weight, int *d_push_reser, int *d_sink_weight,
+int *dPixelLabel, int *dDataTerm, int width, int height, int num_labels);
+
+__device__
+void add_term2(int x, int y, int A, int B, int C, int D, int type, int *d_left_weight,
+int *d_right_weight, int *d_down_weight, int *d_up_weight, int *d_push_reser, int *d_sink_weight);
+
+__device__
+void set_up_expansion_energy_G_ARRAY(int alpha_label, int thid, int *d_left_weight, int *d_right_weight,
+int *d_down_weight, int *d_up_weight, int *d_push_reser,
+int *d_sink_weight, int *dPixelLabel, int *dDataTerm, int *dSmoothTerm,
+int width, int height, int num_labels);
+
+__device__
+void set_up_expansion_energy_G_ARRAY_Cue(int alpha_label, int thid, int *d_left_weight, int *d_right_weight,
+int *d_down_weight, int *d_up_weight, int *d_push_reser,
+int *d_sink_weight, int *dPixelLabel, int *dDataTerm, int *dSmoothTerm,
+int *dHcue, int *dVcue, int width, int height, int num_labels);
+
+
+
+__global__
+void CudaWeightCue(int alpha_label, int *d_left_weight, int *d_right_weight, int *d_down_weight,
+int *d_up_weight, int *d_push_reser, int *d_sink_weight, int *dPixelLabel,
+int *dDataTerm, int *dSmoothTerm, int *dHcue, int *dVcue, int width, int height, int num_labels);
+
+
+__global__
+void CudaWeight(int alpha_label, int *d_left_weight, int *d_right_weight, int *d_down_weight,
+int *d_up_weight, int *d_push_reser, int *d_sink_weight, int *dPixelLabel,
+int *dDataTerm, int *dSmoothTerm, int width, int height, int num_labels);
+
+__global__
+void adjustedgeweight(int *d_left_weight, int *d_right_weight, int *d_down_weight, int *d_up_weight,
+int *d_push_reser, int *d_sink_weight, int *temp_left_weight, int *temp_right_weight,
+int *temp_down_weight, int *temp_up_weight, int *temp_push_reser, int *temp_sink_weight,
+int width, int height, int graph_size, int width1, int height1, int graph_size1);
+
+__global__
+void copyedgeweight(int *d_left_weight, int *d_right_weight, int *d_down_weight, int *d_up_weight,
+int *d_push_reser, int *d_sink_weight, int *temp_left_weight, int *temp_right_weight,
+int *temp_down_weight, int *temp_up_weight, int *temp_push_reser, int *temp_sink_weight,
+int *d_pull_left, int *d_pull_right, int *d_pull_down, int *d_pull_up, int *d_relabel_mask,
+int *d_graph_heightr, int *d_graph_heightw, int width, int height, int graph_size, int width1, int height1, int graph_size1);
+
 #endif
